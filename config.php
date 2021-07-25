0998d861fa8cdcfa01f7582bdd9342179673431c
Navneet Kumar Gupta <navneetg174@gmail.com>
2021-07-25 19:25:40 +0530

    updated the database address to infinity

--- config.php
+++ config.php
@@ -3,10 +3,10 @@
 This file contains database configuration assuming you are running mysql using user "root" and password ""
 */
 
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'SpectrumProject');
+define('DB_SERVER', 'ql300.epizy.com');
+define('DB_USERNAME', 'epiz_29185437');
+define('DB_PASSWORD', '6J6kLN3YRMCzcFE');
+define('DB_NAME', 'epiz_29185437_spectrumproject');
 
 // Try connecting to the Database
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
