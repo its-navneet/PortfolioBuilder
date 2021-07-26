c4a7ede0a1f2602f96b0af81fe212791a72b4f4d
Navneet Kumar Gupta <navneetg174@gmail.com>
2021-07-26 23:55:56 +0530

    Update config.php

--- config.php
+++ config.php
@@ -7,14 +7,9 @@ $pass = 'your password';
 $port = "19938";
 $charset = 'utf8mb4';
 
-$options = [
-    \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
-    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
-    \PDO::ATTR_EMULATE_PREPARES   => false,
-];
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset;port=$port";
 try {
-     $pdo = new \PDO($dsn, $user, $pass, $options);
+     $pdo = new \PDO($dsn, $user, $pass);
      echo "Connected successfully";
 } catch (\PDOException $e) {
      echo "Connection failed: " . $e->getMessage();
