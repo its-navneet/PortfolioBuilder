4000927169d166a04d81e94aece0194ab6f95131
Navneet Kumar Gupta <navneetg174@gmail.com>
2021-07-26 23:53:45 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,19 +1,23 @@
 <?php
-/*
-This file contains database configuration assuming you are running mysql using user "root" and password ""
-*/
 
-define('DB_SERVER', 'mysql-40976-0.cloudclusters.net');
-define('DB_USERNAME', 'admin');
-define('DB_PASSWORD', 'I7DUlO18');
-define('DB_NAME', 'PortfolioBuilder');
+$host = 'mysql-40976-0.cloudclusters.net';
+$db   = 'PortfolioBuilder';
+$user = 'admin';
+$pass = 'your password';
+$port = "19938";
+$charset = 'utf8mb4';
 
-// Try connecting to the Database
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
- 
-// Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+$options = [
+    \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+    \PDO::ATTR_EMULATE_PREPARES   => false,
+];
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset;port=$port";
+try {
+     $pdo = new \PDO($dsn, $user, $pass, $options);
+     echo "Connected successfully";
+} catch (\PDOException $e) {
+     echo "Connection failed: " . $e->getMessage();
 }
+
 ?>
