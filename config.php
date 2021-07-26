58f6e339934aa9edca5c81051868428db20cec64
Navneet Kumar Gupta <navneetg174@gmail.com>
2021-07-26 23:14:03 +0530

    linked to  new database of Cloud cluster

--- config.php
+++ config.php
@@ -3,10 +3,10 @@
 This file contains database configuration assuming you are running mysql using user "root" and password ""
 */
 
-define('DB_SERVER', 'ql300.epizy.com');
-define('DB_USERNAME', 'epiz_29185437');
-define('DB_PASSWORD', '6J6kLN3YRMCzcFE');
-define('DB_NAME', 'epiz_29185437_spectrumproject');
+define('DB_SERVER', 'mysql-40953-0.cloudclusters.net');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'WObNJwOT');
+define('DB_NAME', 'PortfolioBuilder');
 
 // Try connecting to the Database
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
