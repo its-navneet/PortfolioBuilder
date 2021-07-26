e9416eb3e3f24be4e82c83f185ec15b656ac8c44
Navneet Kumar Gupta <navneetg174@gmail.com>
2021-07-27 00:01:39 +0530

    Update config.php

--- config.php
+++ config.php
@@ -5,16 +5,11 @@ $db   = 'PortfolioBuilder';
 $user = 'admin';
 $pass = 'I7DUlO18';
 $port = "19938";
-$charset = 'utf8mb4_0900_ai_ci';
 
-$options = [
-    \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
-    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
-    \PDO::ATTR_EMULATE_PREPARES   => false,
-];
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset;port=$port";
+
+$dsn = "mysql:host=$host;dbname=$db;port=$port";
 try {
-     $pdo = new \PDO($dsn, $user, $pass, $options);
+     $pdo = new \PDO($dsn, $user, $pass);
      echo "Connected successfully";
 } catch (\PDOException $e) {
      echo "Connection failed: " . $e->getMessage();
