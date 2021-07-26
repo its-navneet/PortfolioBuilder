3b16c9136b9a10eccbcb2ba46a272a1cb7d16250
Navneet Kumar Gupta <navneetg174@gmail.com>
2021-07-26 23:59:04 +0530

    Create config.php

--- config.php
+++ config.php
@@ -3,13 +3,18 @@
 $host = 'mysql-40976-0.cloudclusters.net';
 $db   = 'PortfolioBuilder';
 $user = 'admin';
-$pass = 'your password';
+$pass = 'I7DUlO18';
 $port = "19938";
-$charset = 'utf8mb4_0900_ai_ci';
+$charset = 'utf8mb4';
 
+$options = [
+    \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+    \PDO::ATTR_EMULATE_PREPARES   => false,
+];
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset;port=$port";
 try {
-     $pdo = new \PDO($dsn, $user, $pass);
+     $pdo = new \PDO($dsn, $user, $pass, $options);
      echo "Connected successfully";
 } catch (\PDOException $e) {
      echo "Connection failed: " . $e->getMessage();
