cda3f339e66a9096cf2db8dd96ac1ebf1e6bcc2e
Navneet Kumar Gupta <navneetg174@gmail.com>
2021-07-27 21:00:24 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,5 @@
 <?php
-$host = "181.215.242.82";
-$username = "navneet";
-$pass = "navneet2000";
-$link = mysqli_connect("181.215.242.82", "admin", "I7DUlO18", "PortfolioBuilder");
+$link = mysqli_connect("181.215.242.82", "navneet", "navneet2000", "PortfolioBuilder");
 if(mysqli_connect_errno()){
     echo mysqli_connect_errno();
     die();
