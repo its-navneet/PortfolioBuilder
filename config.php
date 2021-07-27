2f6fab28ec367997ea4571dc9c577a0cd980ddd0
Navneet Kumar Gupta <navneetg174@gmail.com>
2021-07-27 20:50:55 +0530

    Update config.php

--- config.php
+++ config.php
@@ -2,10 +2,9 @@
 $host = "181.215.242.82";
 $username = "admin";
 $pass = "I7DUlO18";
-$link = mysqli_connect($host, $username, $pass, "PortfolioBuilder");
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+$link = mysqli_connect("181.215.242.82", "admin", "I7DUlO18", "PortfolioBuilder");
+if(mysqli_connect_errno()){
+    echo mysqli_connect_errno();
+    die();
 }
-
-
 ?>
