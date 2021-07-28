70763da4d6ff2e2c3b382685c278b90345e8750c
Navneet Kumar Gupta <navneetg174@gmail.com>
2021-07-28 22:17:55 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,19 @@
 <?php
-$link = mysqli_connect("181.215.242.82", "admin", "I7DUlO18", "PortfolioBuilder");
-if(mysqli_connect_errno()){
-    echo mysqli_connect_errno();
-    die();
+/*
+This file contains database configuration assuming you are running mysql using user "root" and password ""
+*/
+
+define('DB_SERVER', 'mysql-40976-0.cloudclusters.net');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'I7DUlO18');
+define('DB_NAME', 'PortfolioBuilder');
+
+// Try connecting to the Database
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 ?>
